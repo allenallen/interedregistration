ed2e311efed372a469cd875ee3d9f7c1059eb1d1
Allen Arcenal <edward.arcenal@orangeandbronze.com>
2018-08-16 09:46:39 +0800

    add host

--- intered/settings.py
+++ intered/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '1')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', ''))
 
-ALLOWED_HOSTS = ['intered-web-admin.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['intered-web-admin.herokuapp.com', '127.0.0.1', 'interedregistration.herokuapp.com']
 
 # Application definition
 
