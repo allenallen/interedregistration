65877a0ba7a87b1e5d6b0557240790e8244afdbb
Allen Arcenal <edward.arcenal@orangeandbronze.com>
2018-08-22 15:58:22 +0800

    added required in selects, validation in email

--- registration/forms.py
+++ registration/forms.py
@@ -9,3 +9,16 @@ class RegistrationForm(forms.ModelForm):
         fields = (
             'last_name', 'first_name', 'school', 'shs_track', 'projected_course', 'email', 'date_of_birth', 'gender',
             'mobile')
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['school'].required = True
+        self.fields['shs_track'].required = True
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if Student.objects.filter(email=email).count() > 0:
+            raise forms.ValidationError("Email already exists")
+
+        return email
+
