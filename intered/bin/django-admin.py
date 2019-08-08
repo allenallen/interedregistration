99a9f8c6c0911c18d5156fce32d816c6a52bc5d4
Allen <edward.arcenal@orangeandbronze.com>
2019-08-08 20:45:10 +0800

    Updated logo and title

---
+++ intered/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/allen/Documents/TamarawTechProjects/interedregistration/intered/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
